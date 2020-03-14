4ae4b94034a0fb0c8a12fa1e7ba369c079129a70
Sagar Agarwal <agarwal.sagar@outlook.com>
2020-03-14 20:06:37 +0530

    counter component complete

--- src/275250-sa-example-counter/index.js
+++ src/275250-sa-example-counter/index.js
@@ -1,24 +1,36 @@
 import { createCustomElement } from "@servicenow/ui-core";
 import snabbdom from "@servicenow/ui-renderer-snabbdom";
 import styles from "./styles.scss";
+import style from "./counter.scss";
 
 const view = (state, { updateState }) => {
+	const { tally } = state;
 	return (
-		<div>
+		<div className="counter">
 			<h1>Example counter</h1>
 			<span>
-				<button type="button">Increment</button>
+				<button
+					type="button"
+					on-click={() => updateState({ tally: tally + 1 })}
+				>
+					Increment
+				</button>
 			</span>
 			<span>
-				<button type="button">Reset</button>
+				<button type="button" on-click={() => updateState({ tally: 0 })}>
+					Reset
+				</button>
 			</span>
-			<div>Value: </div>
+			<div>Value: {tally}</div>
 		</div>
 	);
 };
 
 createCustomElement("x-275250-sa-example-counter", {
 	renderer: { type: snabbdom },
+	initialState: {
+		tally: 0
+	},
 	view,
-	styles
+	styles: style
 });
