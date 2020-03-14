8ee34725c4c963c8bc4f4be1a506fe92a0f57f89
Sagar Agarwal <agarwal.sagar@outlook.com>
2020-03-14 19:19:48 +0530

    initiated component fixed for PDI issue

--- example/element.js
+++ example/element.js
@@ -1,8 +1,8 @@
-import '../src/275250-sa-example-counter';
+import "../src/275250-sa-example-counter";
 
-const el = document.createElement('DIV');
+const el = document.createElement("DIV");
 document.body.appendChild(el);
 
 el.innerHTML = `		
-<275250-sa-example-counter></275250-sa-example-counter>
+<x-275250-sa-example-counter></x-275250-sa-example-counter>
 `;
--- src/275250-sa-example-counter/index.js
+++ src/275250-sa-example-counter/index.js
@@ -1,15 +1,13 @@
-import {createCustomElement} from '@servicenow/ui-core';
-import snabbdom from '@servicenow/ui-renderer-snabbdom';
-import styles from './styles.scss';
+import { createCustomElement } from "@servicenow/ui-core";
+import snabbdom from "@servicenow/ui-renderer-snabbdom";
+import styles from "./styles.scss";
 
-const view = (state, {updateState}) => {
-	return (
-		<div></div>
-	);
+const view = (state, { updateState }) => {
+	return <div></div>;
 };
 
-createCustomElement('275250-sa-example-counter', {
-	renderer: {type: snabbdom},
+createCustomElement("x-275250-sa-example-counter", {
+	renderer: { type: snabbdom },
 	view,
 	styles
 });
