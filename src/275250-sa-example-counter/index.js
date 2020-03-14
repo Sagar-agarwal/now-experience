43209f432bcbc554a9841509e7d01ce031f8f9a5
Sagar Agarwal <agarwal.sagar@outlook.com>
2020-03-14 19:24:50 +0530

    HTML part complete

--- src/275250-sa-example-counter/index.js
+++ src/275250-sa-example-counter/index.js
@@ -3,7 +3,18 @@ import snabbdom from "@servicenow/ui-renderer-snabbdom";
 import styles from "./styles.scss";
 
 const view = (state, { updateState }) => {
-	return <div></div>;
+	return (
+		<div>
+			<h1>Example counter</h1>
+			<span>
+				<button type="button">Increment</button>
+			</span>
+			<span>
+				<button type="button">Reset</button>
+			</span>
+			<div>Value: </div>
+		</div>
+	);
 };
 
 createCustomElement("x-275250-sa-example-counter", {
